1324f4ef05eee48faf004cf0600455fc0c817ead
Sheikh091 <musa_zaman09@hotmail.com>
2024-09-14 22:29:55 +0100

    Add files via upload

---
+++ string-data-type.py
@@ -0,0 +1,15 @@
+myString = "This is a string"
+print(myString)
+print(type(myString))
+print(myString + "is of the data type" + str(type(myString)))
+firstString = "water"
+secondString = "fall"
+thirdString = firstString + secondString
+print(thirdString)
+name = input("What is your name?")
+print('Mohammed')
+color = input("What is your favorite color?")
+print ('Green')
+animal = input("What is your favorite animal?")
+print('tiger')
+print("{}, you like a {} {}!".format(name,color,animal))
